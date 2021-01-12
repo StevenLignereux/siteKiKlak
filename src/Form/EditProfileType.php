6716bcbe1d0fed98d7ca9b1fefe01c2268474a2b
Steven <steven.lignereux@hotmail.fr>
2021-01-12 22:35:54 +0100

    Final commit for presentation

--- src/Form/EditProfileType.php
+++ src/Form/EditProfileType.php
@@ -9,7 +9,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Regex;
 
 class EditProfileType extends AbstractType
 {
@@ -21,21 +20,14 @@ class EditProfileType extends AbstractType
                     new NotBlank([
                         'message' => 'Veuillez entrer votre nom',
                     ]),
-                    new Regex([
-                        'pattern' => '/^[a-z]+$/i',
-                        'message' => 'Votre nom ne doit pas contenir de caractères spéciaux ou de chiffres'
-                    ])
                 ]
             ])
             ->add('firstname', TextType::class, [
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Veuillez entrer votre nom',
+                        'message' => 'Veuillez entrer votre Prénom',
                     ]),
-                    new Regex([
-                        'pattern' => '/^[a-z]+$/i',
-                        'message' => 'Votre prénom ne doit pas contenir de caractères spéciaux ou de chiffres'
-                    ])
+
                 ]
             ])
             ->add('email', EmailType::class, [
